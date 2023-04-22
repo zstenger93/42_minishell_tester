6033e4c5ccc177a541cf7bbc41027f572fba4f08
kvebers <kvebers@student.42heilbronn.de>
2023-04-22 16:12:57 +0200

    Parsing HELL

---
+++ cmds/mand/10_parsing_hell.sh
@@ -0,0 +1,219 @@
+# **************************************************************************** #
+#                                PARSING HELL                                  #
+# **************************************************************************** #
+
+"echo "
+
+"echo -nnnnn"
+
+"ECho" -n -nnn"" "-"nnnnn
+
+"ECHO" "-n"
+
+ECHO '''''-''n'
+
+echo '' -n
+
+echo "" "" "" -n -n -n -n
+
+export T=n
+echo "-"$T$T
+
+export T=ech
+echo $To
+
+export T=ech
+echo $T"o"
+
+export T=ech
+echo $T"o "
+
+export T=ech
+echo $T"o -n"
+
+export T=ech
+echo $T"o -n"
+
+export T=ech
+echo $T'o'
+
+export T="-n test1 -n test 2"
+echo $T
+
+export T=ech
+echo $T'o '
+
+"ECHO"
+
+'echo'
+
+'PWD'
+
+"PWD"
+
+echo "-nnnnn "
+
+ECHO -nn
+
+ECHO -n -n
+
+ECHO "-"""n""n""n""n""n""nnnnnn-n
+
+ECHO "-"""n""n""n""n""n""nnnnnn -n
+
+export T=nnnnnnnn
+echo "-""$T"nnnnnnnnnnnnn -nnnnnnnn"$T" '-'"$T"
+
+export T=nnnnnnnn
+echo "-""$T"nnnnnnnnnnnnn -nnnnnnnn"$T" '-''$T'
+
+export T=nnnnnnnn
+echo "-"'$T'nnnnnnnnnnnnn -nnnnnnnn'$T' '-'"$T "
+
+export T=nnnnnnnn
+echo "-"'$T'nnnnnnnnnnnnn -nnnnnnnn$T '-''$T '
+
+export T=e E=c S=h L=o
+$T$E$S$L
+
+export T=e E=c S=h L=o L=ok
+$T$E$S$L
+
+export T=e E=c S=h L=o
+$T$E$S$L -n
+
+export T=E E=c S=h L=o
+$T$E$S$L -n
+
+export T=E E=c S=h L=o
+$T$E$S$L -nn
+
+export T=e E=c S=h L=o
+$T$E$S$L -nn
+
+export T=e E=c S=h L=o
+$T$E"c"$L -nn
+
+export T=e E=c S=h L=o
+$T$E"C"$L -nn
+
+export T=e E=c S=h L=o
+$T$E"C"$L "" -""n
+
+LS
+
+Ls
+
+lS
+
+ls
+
+PWD
+
+pWD
+
+pwd
+
+pwD
+
+export T="|"
+echo segfault $T grep segfault
+
+export T='|'
+echo segfault $T grep segfault
+
+export T=">"
+echo segfault $T grep segfault
+
+export T='<'
+echo segfault $T grep segfault
+
+export T="<<"
+echo segfault $T grep segfault
+
+export T='<<'
+echo segfault $T grep segfault
+
+export T="|"
+$T$T$T$T$T$T$T
+
+export T="echo segfault | grep segfault"
+$T
+
+export T=-nnnnnnnn"nnnnnnn "
+echo $T
+
+export T=-"n                    "
+Echo $T
+
+EXIT 123
+
+Exit
+
+UNSET USER
+
+Unset USER
+
+EXPORT T=LA
+echo $T
+
+Export T=LA
+echo $T
+
+"e"'x''p''o''r''t' L=T
+echo $T
+
+e""""""""""""cho
+
+export T='|'
+echo $T echo lala $T echo $T echo ?
+
+export T="<<"
+$T.
+
+OLDPWD
+
+USER
+
+" ls"
+
+" cat"
+
+" echo"
+
+" PWD"
+
+" pwd"
+
+"PWD "
+
+"pwd "
+
+'pwd '
+
+p""''''w''''''""""""''''''''''''''''''''''''''''''''d
+
+'''''''''''''''' echo lol
+
+           ''echo lol
+
+           ""echo lol
+
+           ""echo"" lol
+
+           ""'echo'"" lol
+
+echo "         |       "  | echo maybe
+
+echo "|"  | echo maybe
+
+echo '|'  | echo maybe
+
+echo -nnnnnnnnnnnn
+
+export L=amazing L=lol L=cool
+echo $L
+
+export L=amazing L=lol L=cool
+export L=lol
+echo $L
