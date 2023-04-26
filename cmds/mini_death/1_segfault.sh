d1509b4f391cab5e459dbd6340d9b2035f1ca5fe
zstenger <zstenger@student.42heilbronn.de>
2023-04-26 13:39:59 +0200

    death update

--- cmds/mini_death/1_segfault.sh
+++ cmds/mini_death/1_segfault.sh
@@ -1,36 +1,28 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    1_segfault.sh                                      :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: kvebers <kvebers@student.42.fr>            +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/04/26 13:05:49 by kvebers           #+#    #+#              #
-#    Updated: 2023/04/26 13:17:03 by kvebers          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
-echo seg <> echo seg
-
-echo seg >< echo segf
-
-echo seg <<> echo segf
-
-echo seg <<<> echo segf
-
-echo <<< echo seegf
-
 echo |< echo segf
 
 echo segf >| echo is this invalid
 
-echo segf <|< echo super valid
+echo >          >           < "echo"
+
+echo >          >         | echo kekw
+
+echo >          >         | echo super valid
+
+echo <          <         > echo
+
+echo <  < <         > ok
+
+echo <  < | echo ok
+
+echo <  < |    < ok
+
+echo <      < |    > echo
+
+echo >>| echo super valid
 
 echo >>< "echo"
 
 echo >>| echo kekw
-
-echo >>| echo super valid
 
 echo <<> echo
 
@@ -98,22 +90,19 @@ echo segf >     | echo is this invalid
 
 echo segf <         |        < echo super valid
 
-echo >          >           < "echo"
-
-echo >          >         | echo kekw
-
-echo >          >         | echo super valid
-
-echo <          <         > echo
-
-echo <  < <         > ok
-
-echo <  < | echo ok
-
-echo <  < |    < ok
-
-echo <      < |    > echo
 
 echo segfault <"    <   <   <"  <   <   amazing
 .
 amazing
+
+echo seg <> echo seg
+
+echo seg >< echo segf
+
+echo seg <<> echo segf
+
+echo seg <<<> echo segf
+
+echo <<< echo seegf
+
+echo segf <|< echo super valid
