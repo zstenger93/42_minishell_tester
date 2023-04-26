012d68c77db7ecbc1bfe818b19f5a1f92f8820c6
zstenger <zstenger@student.42heilbronn.de>
2023-04-26 11:17:21 +0200

    tmp removing some cases

--- cmds/mand/10_parsing_hell.sh
+++ cmds/mand/10_parsing_hell.sh
@@ -307,29 +307,15 @@ cd --
 
 echo seg <> echo seg
 
-echo seg >< echo segf
-
-echo seg <<> echo segf
-
-echo seg <<<> echo segf
-
-echo <<< echo seegf
-
 echo |< echo segf
 
 echo segf >| echo is this invalid
 
 echo segf <|< echo super valid
 
-echo >>< "echo"
-
 echo >>| echo kekw
 
 echo >>| echo super valid
-
-echo <<> echo
-
-echo <<<> ok
 
 echo <<| echo ok
 
