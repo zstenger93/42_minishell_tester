96482ff7cbe969c4f4106b56184cb9ed02c59edf
Sotir Danailov <sndanailov@gmail.com>
2024-03-15 10:12:48 +0100

    when using rand() here, it's impossible to pass since it's, well, random

--- cmds/mand/1_scmds.sh
+++ cmds/mand/1_scmds.sh
@@ -41,7 +41,7 @@ awk '$1 ~ /^d/ {print $9}'
 
 awk 'BEGIN{for(i=1;i<=10;i++){for(j=1;j<=10;j++){printf("%4d ",i*j)} printf("\n")}}' /dev/null | tail -n 10
 
-awk 'BEGIN{for(i=1;i<=1000000;i++)print int(rand()*1000)}' | awk '{sum+=$1} END {print sum/NR}'
+awk 'BEGIN{for(i=1;i<=1000000;i++)print int(i)}' | awk '{sum+=$1} END {print sum/NR}'
 
 asd
 echo asd
